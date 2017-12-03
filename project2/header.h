e1f848e4d8450df445bdcdd65d9186ba507be1b6
heoboseong7 <boseong844@gmail.com>
2017-12-03 23:07:52 +0900

    reverse and header

--- project2/header.h
+++ project2/header.h
@@ -1,1 +1,11 @@
 #pragma once
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+
+void T_trivial(int n, int d, int length, char str[]);
+void Juggling(char ary[], int d, int n);
+void swap(char arr[], int a, int b, int d);
+void block_swap(char arr[], int n, int d);
+void reverse(int arr[], int start, int end);
+void _reverse(int arr[], int d, int n);
