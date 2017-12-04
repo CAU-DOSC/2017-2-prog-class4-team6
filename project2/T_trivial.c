b3a31d7d1fb889ea98b85bff54964426fa4dfd54
rlacksgus97 <rlacksgus97@naver.com>
2017-12-04 21:33:15 +0900

    update for T_trivial

--- project2/T_trivial.c
+++ project2/T_trivial.c
@@ -1,35 +1,32 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 
-void T_trivial(int n, int d, int length, char str[])
+void T_trivial(int n, int d, char str[])
 {
 	if (d > 0)
 	{
-		for (int i = 0; i < n ; i++)
+		for (int i = 0; i < d ; i++)
 		{
-			char temp = str[length-1];
-			str[i + 1] = str[i];
+			char temp = str[n-1];
+			for (int j = n-1; j > 0; j--)
+			{
+				str[j] = str[j - 1];
+				
+			}
 			str[0] = temp;
 		}
 	}
 	else if (d < 0)
 	{
-		for (int i = 0; i < n; i++)
+		for (int i = 0; i < -d; i++)
 		{
 			char temp = str[0];
-			str[i] = str[i + 1];
-			str[length-1] = temp;
+			for (int j = 0; j < (n-1); j++)
+			{
+				str[j] = str[j + 1];
+
+			}
+			str[n-1] = temp;
 		}
 	}
-}
-
-int main()
-{
-	int n, d, length = 0;
-	char str[100] = { 'a','b','c' };
-	scanf("%d %d %d", &n, &d, &length);
-	printf("%d %d %d\n", n, d, length);
-	T_trivial(n, d, length, str);
-	printf("%c", str[0]);
-	return 0;
 }
