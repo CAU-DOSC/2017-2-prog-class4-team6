5154c9084b26785fd659f6fe26cbea2a5c2a1ada
heoboseong8 <33649907+heoboseong8@users.noreply.github.com>
2017-12-04 21:31:10 +0900

    reverse change

--- project2/header.h
+++ project2/header.h
@@ -8,6 +8,6 @@ void T_trivial(int n, int d, int length, char str[]);
 void Juggling(char ary[], int d, int n);
 void swap(char arr[], int a, int b, int d);
 void block_swap(char arr[], int n, int d);
-void reverse(int arr[], int start, int end);
-void _reverse(int arr[], int d, int n);
+void reverse(char arr[], int start, int end);
+void _reverse(char arr[], int d, int n);
 int gcd(int a, int b);
