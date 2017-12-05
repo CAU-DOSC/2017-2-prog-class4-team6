7f8a10e7e80ce2eefdfceed8a4241a38708c370f
wnsgud0428 <33649857+wnsgud0428@users.noreply.github.com>
2017-12-05 14:19:57 +0900

    Update header.h

--- project2/header.h
+++ project2/header.h
@@ -4,7 +4,7 @@
 #include <stdlib.h>
 #include <time.h>
 
-void T_trivial(int n, int d, int length, char str[]);
+void T_trivial(int n, int d, char str[]);
 void Juggling(char ary[], int d, int n);
 void swap(char arr[], int a, int b, int d);
 void block_swap(char arr[], int n, int d);
